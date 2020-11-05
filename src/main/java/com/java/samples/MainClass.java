e182e07d2e619bd1c34c4710071c874a3fbd926e
Barandeep Singh <barandeepsingh@gmail.com>
2020-11-05 23:16:38 +0530

    Added lombok support

--- src/main/java/com/java/samples/MainClass.java
+++ src/main/java/com/java/samples/MainClass.java
@@ -1,7 +1,10 @@
 package com.java.samples;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Test");
+        log.info("Test");
     }
 }
